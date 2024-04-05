c5d6c9529c54b014b646b3cfaf090a879fdccc4f
guilherme-torres <guilhermetorres1401@gmail.com>
2024-04-05 06:52:03 -0300

    adicionar chaves estrangeiras

--- criar_tabelas.sql
+++ criar_tabelas.sql
@@ -4,16 +4,16 @@ CREATE TABLE IF NOT EXISTS Empresas (
     natureza_juridica VARCHAR(4),
     qualificacao_do_responsavel VARCHAR(2),
     capital_social_da_empresa VARCHAR(20),
-    porte_da_empresa VARCHAR(2),
+    porte_da_empresa SMALLINT,
     ente_federativo_responsavel VARCHAR(100)
 );
 CREATE TABLE IF NOT EXISTS Estabelecimentos (
     cnpj_basico VARCHAR(8),
     cnpj_ordem VARCHAR(4),
     cnpj_dv VARCHAR(2),
-    identificador_matriz_filial VARCHAR(1),
+    identificador_matriz_filial SMALLINT,
     nome_fantasia VARCHAR(100),
-    situacao_cadastral VARCHAR(2),
+    situacao_cadastral SMALLINT,
     data_situacao_cadastral VARCHAR(8),
     motivo_situacao_cadastral VARCHAR(2),
     nome_da_cidade_no_exterior VARCHAR(60),
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS Simples (
 );
 CREATE TABLE IF NOT EXISTS Socios (
     cnpj_basico VARCHAR(8),
-    identificador_de_socio VARCHAR(1),
+    identificador_de_socio SMALLINT,
     nome_do_socio_razao_social VARCHAR(255),
     cnpj_cpf_do_socio VARCHAR(14),
     qualificacao_do_socio VARCHAR(2),
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS Socios (
     representante_legal VARCHAR(11),
     nome_do_representante VARCHAR(255),
     qualificacao_do_representante_legal VARCHAR(2),
-    faixa_etaria VARCHAR(1)
+    faixa_etaria SMALLINT
 );
 CREATE TABLE IF NOT EXISTS Cnaes (
     codigo VARCHAR(7) NOT NULL PRIMARY KEY,
@@ -86,56 +86,71 @@ CREATE TABLE IF NOT EXISTS Qualificacoes (
     descricao VARCHAR(100)
 );
 CREATE TABLE IF NOT EXISTS PorteEmpresa (
-    codigo VARCHAR(2),
+    codigo SMALLINT NOT NULL PRIMARY KEY,
     descricao VARCHAR(30)
 );
 INSERT INTO PorteEmpresa (codigo, descricao) VALUES
-('00', 'NÃO INFORMADO'),
-('01', 'MICRO EMPRESA'),
-('03', 'EMPRESA DE PEQUENO PORTE'),
-('05', 'DEMAIS');
+(0, 'NÃO INFORMADO'),
+(1, 'MICRO EMPRESA'),
+(3, 'EMPRESA DE PEQUENO PORTE'),
+(5, 'DEMAIS');
 CREATE TABLE IF NOT EXISTS MatrizFilial (
-    codigo VARCHAR(1),
+    codigo SMALLINT NOT NULL PRIMARY KEY,
     descricao VARCHAR(10)
 );
 INSERT INTO MatrizFilial (codigo, descricao) VALUES
-('1', 'MATRIZ'),
-('2', 'FILIAL');
+(1, 'MATRIZ'),
+(2, 'FILIAL');
 CREATE TABLE IF NOT EXISTS SituacaoCadastral (
-    codigo VARCHAR(2),
+    codigo SMALLINT NOT NULL PRIMARY KEY,
     descricao VARCHAR(10)
 );
 INSERT INTO SituacaoCadastral (codigo, descricao) VALUES
-('01', 'NULA'),
-('2', 'ATIVA'),
-('3', 'SUSPENSA'),
-('4', 'INAPTA'),
-('08', 'BAIXADA');
+(1, 'NULA'),
+(2, 'ATIVA'),
+(3, 'SUSPENSA'),
+(4, 'INAPTA'),
+(8, 'BAIXADA');
 CREATE TABLE IF NOT EXISTS IdentificacaoSocio (
-    codigo VARCHAR(1),
+    codigo SMALLINT NOT NULL PRIMARY KEY,
     descricao VARCHAR(20)
 );
 INSERT INTO IdentificacaoSocio (codigo, descricao) VALUES
-('1', 'PESSOA JURÍDICA'),
-('2', 'PESSOA FÍSICA'),
-('3', 'ESTRANGEIRO');
+(1, 'PESSOA JURÍDICA'),
+(2, 'PESSOA FÍSICA'),
+(3, 'ESTRANGEIRO');
 CREATE TABLE IF NOT EXISTS FaixaEtaria (
-    codigo VARCHAR(2),
+    codigo SMALLINT NOT NULL PRIMARY KEY,
     descricao VARCHAR(20)
 );
 INSERT INTO FaixaEtaria (codigo, descricao) VALUES
-('1', '0-12'),
-('2', '13-20'),
-('3', '21-30'),
-('4', '31-40'),
-('5', '41-50'),
-('6', '51-60'),
-('7', '61-70'),
-('8', '71-80'),
-('9', '+80'),
-('0', 'não se aplica');
+(1, '0-12'),
+(2, '13-20'),
+(3, '21-30'),
+(4, '31-40'),
+(5, '41-50'),
+(6, '51-60'),
+(7, '61-70'),
+(8, '71-80'),
+(9, '+80'),
+(0, 'NÃO SE APLICA');
 CREATE TABLE IF NOT EXISTS HashArquivos (
     nome VARCHAR(30) NOT NULL,
     hash VARCHAR(64) NOT NULL,
     ultima_modificacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+ALTER TABLE Empresas ADD CONSTRAINT fk_natureza_juridica FOREIGN KEY (natureza_juridica) REFERENCES Naturezas(codigo);
+ALTER TABLE Empresas ADD CONSTRAINT fk_porte_da_empresa FOREIGN KEY (porte_da_empresa) REFERENCES PorteEmpresa(codigo);
+ALTER TABLE Empresas ADD CONSTRAINT fk_qualificacao_do_responsavel FOREIGN KEY (qualificacao_do_responsavel) REFERENCES Qualificacoes(codigo);
+ALTER TABLE Estabelecimentos ADD CONSTRAINT fk_identificador_matriz_filial FOREIGN KEY (identificador_matriz_filial) REFERENCES MatrizFilial(codigo);
+ALTER TABLE Estabelecimentos ADD CONSTRAINT fk_situacao_cadastral FOREIGN KEY (situacao_cadastral) REFERENCES SituacaoCadastral(codigo);
+ALTER TABLE Estabelecimentos ADD CONSTRAINT fk_motivo_situacao_cadastral FOREIGN KEY (motivo_situacao_cadastral) REFERENCES Motivos(codigo);
+ALTER TABLE Estabelecimentos ADD CONSTRAINT fk_pais FOREIGN KEY (pais) REFERENCES Paises(codigo);
+ALTER TABLE Estabelecimentos ADD CONSTRAINT fk_cnae_fiscal_principal FOREIGN KEY (cnae_fiscal_principal) REFERENCES Cnaes(codigo);
+ALTER TABLE Estabelecimentos ADD CONSTRAINT fk_cnae_fiscal_secundaria FOREIGN KEY (cnae_fiscal_secundaria) REFERENCES Cnaes(codigo);
+ALTER TABLE Estabelecimentos ADD CONSTRAINT fk_municipio FOREIGN KEY (municipio) REFERENCES Municipios(codigo);
+ALTER TABLE Socios ADD CONSTRAINT fk_identificador_de_socio FOREIGN KEY (identificador_de_socio) REFERENCES IdentificacaoSocio(codigo);
+ALTER TABLE Socios ADD CONSTRAINT fk_qualificacao_do_socio FOREIGN KEY (qualificacao_do_socio) REFERENCES Qualificacoes(codigo);
+ALTER TABLE Socios ADD CONSTRAINT fk_pais FOREIGN KEY (pais) REFERENCES Paises(codigo);
+ALTER TABLE Socios ADD CONSTRAINT fk_qualificacao_do_representante_legal FOREIGN KEY (qualificacao_do_representante_legal) REFERENCES Qualificacoes(codigo);
+ALTER TABLE Socios ADD CONSTRAINT fk_faixa_etaria FOREIGN KEY (faixa_etaria) REFERENCES FaixaEtaria(codigo);
