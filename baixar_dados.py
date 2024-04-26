8226cc893d032f9e913f52e61a840bcb2565d3d6
guilherme-torres <guilhermetorres1401@gmail.com>
2024-04-26 12:02:53 -0300

    download multithreading

--- baixar_dados.py
+++ baixar_dados.py
@@ -3,10 +3,33 @@ import requests
 import zipfile
 import os
 import tqdm
+import threading
+
+def download(link, filename):
+    resposta = requests.get(link, stream=True)
+    arquivo_zip = filename
+    if resposta.status_code == 200:
+        with open(arquivo_zip, 'wb') as arquivo:
+            total = int(resposta.headers.get('content-length', 0))
+            tqdm_params = {
+                'desc': link,
+                'total': total,
+                'miniters': 1,
+                'unit': 'B',
+                'unit_scale': True,
+                'unit_divisor': 1024,
+            }
+            with tqdm.tqdm(**tqdm_params) as pb:
+                for chunk in resposta.iter_content(chunk_size=65536):
+                    pb.update(len(chunk))
+                    arquivo.write(chunk)
+    else:
+        resposta.raise_for_status()
+
 
 def baixar_dados():
     url = 'https://dadosabertos.rfb.gov.br/CNPJ/'
-    destino = os.path.join(os.getcwd(), 'dados')
+    destino = os.path.join(os.getcwd(), 'dados_teste')
 
     if not os.path.exists(destino):
         os.makedirs(destino)
@@ -18,41 +41,48 @@ def baixar_dados():
     for link in pagina.find_all('a'):
         href = link.get('href')
         if href.endswith('.zip'):
-            links.append(url + href)
+            links.append((url + href, os.path.join(destino, href)))
 
-    for link in links:
-        resposta = requests.get(link, stream=True)
-        arquivo_zip = os.path.join(destino, link[37:])
-        if resposta.status_code == 200:
-            with open(arquivo_zip, 'wb') as arquivo:
-                total = int(resposta.headers.get('content-length', 0))
-                tqdm_params = {
-                    'desc': link,
-                    'total': total,
-                    'miniters': 1,
-                    'unit': 'B',
-                    'unit_scale': True,
-                    'unit_divisor': 1024,
-                }
-                with tqdm.tqdm(**tqdm_params) as pb:
-                    for chunk in resposta.iter_content(chunk_size=32768):
-                        pb.update(len(chunk))
-                        arquivo.write(chunk)
-        else:
-            resposta.raise_for_status()
+    threads = []
+    for link, filename in links:
+        thread = threading.Thread(target=download, args=(link, filename))
+        thread.start()
+        threads.append(thread)
+    
+    for thread in threads:
+        thread.join()
+        # resposta = requests.get(link, stream=True)
+        # arquivo_zip = os.path.join(destino, link[37:])
+        # if resposta.status_code == 200:
+        #     with open(arquivo_zip, 'wb') as arquivo:
+        #         total = int(resposta.headers.get('content-length', 0))
+        #         tqdm_params = {
+        #             'desc': link,
+        #             'total': total,
+        #             'miniters': 1,
+        #             'unit': 'B',
+        #             'unit_scale': True,
+        #             'unit_divisor': 1024,
+        #         }
+        #         with tqdm.tqdm(**tqdm_params) as pb:
+        #             for chunk in resposta.iter_content(chunk_size=32768):
+        #                 pb.update(len(chunk))
+        #                 arquivo.write(chunk)
+        # else:
+        #     resposta.raise_for_status()
 
-    print('Descompactando arquivos...')
-    for link in links:
-        nome_arquivo = link[37:]
-        arquivo_zip = os.path.join(destino, nome_arquivo)
-        with zipfile.ZipFile(arquivo_zip, 'r') as zip_file:
-            novo_nome = os.path.join(os.path.join(destino, 'csv'), nome_arquivo.split('.')[0] + '.csv')
-            if os.path.exists(novo_nome):
-                os.remove(novo_nome)
-            nome_original = os.path.join(os.path.join(destino, 'csv'), zip_file.namelist()[0])
-            zip_file.extractall(os.path.join(destino, 'csv'))
-            os.rename(nome_original, novo_nome)
-    print('Arquivos descompactados com sucesso')
+    # print('Descompactando arquivos...')
+    # for link in links:
+    #     nome_arquivo = link[37:]
+    #     arquivo_zip = os.path.join(destino, nome_arquivo)
+    #     with zipfile.ZipFile(arquivo_zip, 'r') as zip_file:
+    #         novo_nome = os.path.join(os.path.join(destino, 'csv'), nome_arquivo.split('.')[0] + '.csv')
+    #         if os.path.exists(novo_nome):
+    #             os.remove(novo_nome)
+    #         nome_original = os.path.join(os.path.join(destino, 'csv'), zip_file.namelist()[0])
+    #         zip_file.extractall(os.path.join(destino, 'csv'))
+    #         os.rename(nome_original, novo_nome)
+    # print('Arquivos descompactados com sucesso')
 
 
 if __name__ == '__main__':
